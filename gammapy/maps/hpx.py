ea0d7a471c216e2977bbdf643e6a8a031b0f7178
Axel Donath <axel.donath@mpi-hd.mpg.de>
2021-04-02 16:28:18 +0200

    Implement HpxGeom.cutout and HpxGeom.to_binsz

--- gammapy/maps/hpx.py
+++ gammapy/maps/hpx.py
@@ -891,6 +891,63 @@ class HpxGeom(Geom):
         """
         return self._center_skydir
 
+    def cutout(self, position, width, **kwargs):
+        """Create a cutout around a given position.
+
+        Parameters
+        ----------
+        position : `~astropy.coordinates.SkyCoord`
+            Center position of the cutout region.
+        width : `~astropy.coordinates.Angle` or `~astropy.units.Quantity`
+            Radius of the circular cutout region.
+
+        Returns
+        -------
+        cutout : `~gammapy.maps.WcsNDMap`
+            Cutout map
+        """
+        if not self.is_allsky:
+            raise ValueError("Can only do a cutout from an allsky map.")
+
+        if not self.is_regular:
+            raise ValueError("Can only do a cutout from a regular map.")
+
+        width = u.Quantity(width, "deg").value
+        return self.create(
+            nside=self.nside,
+            nest=self.nest,
+            width=width,
+            skydir=position,
+            frame=self.frame,
+            axes=self.axes
+        )
+
+    def is_aligned(self, other):
+        """Check if HEALPIx geoms and extra axes are aligned.
+
+        Parameters
+        ----------
+        other : `HpxGeom`
+            Other geom.
+
+        Returns
+        -------
+        aligned : bool
+            Whether geometries are aligned
+        """
+        for axis, otheraxis in zip(self.axes, other.axes):
+            if axis != otheraxis:
+                return False
+
+        if not self.nside == other.nside:
+            return False
+        elif not self.frame == other.frame:
+            return False
+        elif not self.nest == other.nest:
+            return False
+        else:
+            return True
+
     @property
     def ipix(self):
         """HEALPIX pixel and band indices for every pixel in the map."""
@@ -981,7 +1038,7 @@ class HpxGeom(Geom):
             return self.create(
                 skydir=self.center_skydir,
                 binsz=binsz,
-                width=self._get_region_size(),
+                width=self.width.to_value("deg"),
                 frame=self.frame,
                 axes=copy.deepcopy(self.axes),
             )
@@ -1419,19 +1476,21 @@ class HpxGeom(Geom):
 
         return get_hpxregion_dir(self.region, self.frame)
 
-    def _get_region_size(self):
+    @property
+    def width(self):
+        """Width of the HEALPix geom"""
         import healpy as hp
 
         if self.region is None:
-            return 180.0
+            return 180.0 * u.deg
         if self.region == "explicit":
             idx = unravel_hpx_index(self._ipix, self._maxpix)
             nside = self._get_nside(idx)
             ang = hp.pix2ang(nside, idx[0], nest=self.nest, lonlat=True)
             dirs = SkyCoord(ang[0], ang[1], unit="deg", frame=self.frame)
-            return np.max(dirs.separation(self.center_skydir).deg)
+            return np.max(dirs.separation(self.center_skydir).deg) * u.deg
 
-        return get_hpxregion_size(self.region)
+        return get_hpxregion_size(self.region) * u.deg
 
     def _get_nside(self, idx):
         if self.nside.size > 1:
@@ -1468,7 +1527,7 @@ class HpxGeom(Geom):
         """
         pix_size = get_pix_size_from_nside(self.nside)
         binsz = np.min(pix_size) / oversample
-        width = 2.0 * self._get_region_size() + np.max(pix_size)
+        width = 2.0 * self.width.to_value("deg") + np.max(pix_size)
 
         if width_pix is not None and int(width / binsz) > width_pix:
             binsz = width / width_pix
